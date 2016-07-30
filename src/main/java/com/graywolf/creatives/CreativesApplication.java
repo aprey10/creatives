39a3fafb43b8dfb48ac56054636da490ac7ce56f
graywolf <glory.ukraine10@gmail.com>
2016-07-30 15:27:36 +0300

    add maven project based on Spring Bootsrap with initial configuration

---
+++ src/main/java/com/graywolf/creatives/CreativesApplication.java
@@ -0,0 +1,12 @@
+package com.graywolf.creatives;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreativesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreativesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/graywolf/creatives/CreativesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.graywolf.creatives;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CreativesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
